d6981b48338a030b7c7c5582bb4cb689d075526f
Miguelfr1 <90462139+Miguelfr1@users.noreply.github.com>
2024-03-14 18:41:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+    var content = document.getElementById("content");
+    var background = document.getElementById("background");
+
+    // Masquer le contenu
+    content.style.display = "none";
+    // Afficher le fond d'écran
+    background.style.backgroundImage = "url('./img/fondflappy.jpg')";
+}
+
+
